2b1eb5df625f1d99e7cdacb91189e9fcee3e40fc
CWT22 <97423003+CWT22@users.noreply.github.com>
2022-01-14 11:34:25 +0800

    Delete Myproject directory

--- Myproject/myapi/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from .models import Article
-from .serializers import ArticleSerializer
-
-# Create your views here.
